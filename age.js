a2a6a1cf1ae799683d16eb2536ad92bbb5d375bb
salammustafa728 <salammustafa728@gmail.com>
2021-04-01 14:04:26 +0300

    add a js files to take the user name,age,color then change the body color

---
+++ age.js
@@ -0,0 +1,10 @@
+var age = window.prompt("Enter your age please: ; 'Age'");
+if (age >=10){
+    alert('Hello, Your age is ' + age);
+    document.write(age);
+    console.log(age);
+}else if(age<10 && age>=0){
+    alert('Your are too young ');
+}else{
+    alert('Please enter a valid age');
+}
---
+++ changecolor.js
@@ -0,0 +1,12 @@
+function change(){
+  
+    var txt;
+var r = confirm("change background color!");
+if (r == true) {
+  txt = "You pressed OK!";
+  document.body.style.background = color;
+} else {
+  txt = "You pressed Cancel!";
+}
+    
+}
